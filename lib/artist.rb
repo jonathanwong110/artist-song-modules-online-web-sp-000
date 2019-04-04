3ce347c148b9aeb561bb6260b0eadc4bef844a20
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-04 19:14:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,28 +3,22 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
+  
+  extend Findable::ClassMethods
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+  
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -40,3 +34,4 @@ class Artist
     name.downcase.gsub(' ', '-')
   end
 end
+
